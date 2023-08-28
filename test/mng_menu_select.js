d48cf923c102e71c83e9aa9063ef6439e83ad3fb
monena999 <139050931+monena999@users.noreply.github.com>
2023-08-28 16:32:51 +0900

    コード本体です

---
+++ test/mng_menu_select.js
@@ -0,0 +1,24 @@
+$("#botan1").on("click", function(){
+ location.href = "./em_ management.html"
+ 
+ });
+ 
+$("#botan2").on("click", function(){
+ location.href = "./work_p_management.html"
+ 
+ });
+ 
+$("#botan3").on("click", function(){
+ location.href = "./em_workplace.html"
+ 
+ });
+ 
+$("#botan4").on("click", function(){
+ location.href = "./calendar.html"
+ 
+ });
+ 
+$("#botan5").on("click", function(){
+ location.href = "./title_top.html"
+ 
+ });
---
+++ test/title_top.js
@@ -0,0 +1,9 @@
+$("#time_keisan").on("click", function(){
+ location.href = "./ym_selet.html"
+ 
+ });
+ 
+ $("#kanri_keisan").on("click", function(){
+ location.href = "./mng_menu_select.html"
+
+ });
